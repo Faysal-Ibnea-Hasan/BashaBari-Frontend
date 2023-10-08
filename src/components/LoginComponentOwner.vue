0261354ec5152f30c9c8331b81b2d2bc7d81ec3e
unknown <foysal.jesan@gmail.com>
2023-10-08 11:58:46 +0600

    Owner Authentication Done

--- src/components/LoginComponentOwner.vue
+++ src/components/LoginComponentOwner.vue
@@ -1,35 +1,68 @@
 <template>
-    <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <div class="text-center ms-4 lg:text-left">
-      <h1 class="text-5xl font-bold">Login please!</h1>
-      
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <div class="text-center ms-4 lg:text-left">
+            <h1 class="text-5xl font-bold">Login please!</h1>
+
+        </div>
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 hover:-translate-y-1 hover:scale-110 duration-500">
+            <div class="card-body">
+              <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Mobile Number</span>
+                    </label>
+                    <input type="text" placeholder="Mobile number" v-model="mobile" class="input input-bordered" required />
+                </div>
+                <!-- <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" v-model="password" class="input input-bordered" required />
+
+                </div> -->
+                <div class="form-control mt-6">
+                    <button v-on:click="login" class="btn btn-primary">Login</button>
+                </div>
+            </div>
+                
+            
+        </div>
     </div>
-    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 hover:-translate-y-1 hover:scale-110 duration-500">
-      <form class="card-body">
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" class="input input-bordered" required />
-        </div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" class="input input-bordered" required />
-         
-        </div>
-        <div class="form-control mt-6">
-          <router-link to="/home-owner"><button class="btn btn-primary">Login</button></router-link>
-        </div>
-      </form>
-    </div>
-  </div>
 </div>
 </template>
+
 <script>
-    export default {
-        name:"LoginComponentOwner"
+import axios from 'axios'
+export default {
+    name: "LoginComponentOwner",
+    data() {
+        return {
+          mobile:'',
+          password:''
+        }
+    },
+    methods: {
+        async login() {
+            let result = await axios.post(
+                `http://127.0.0.1:8000/api/Api/Owner/Check`,{
+
+                  mobile: this.mobile
+                }
+            )
+
+             console.warn(this.mobile,result)
+            if (result.data.status == true) {
+                
+                this.$router.push({
+                    name: 'HomePageOwner'
+                })
+            }
+            else if(result.data.status == false) {
+              this.$router.push({
+                    name: 'HelloWorld'
+                })
+            }
+        }
     }
+}
 </script>
