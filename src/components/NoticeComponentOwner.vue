c6cde93f4157d28fcafa86d59ccc5dd7fc719ad6
unknown <foysal.jesan@gmail.com>
2023-10-24 18:49:58 +0600

    Notice Component Owner

--- src/components/NoticeComponentOwner.vue
+++ src/components/NoticeComponentOwner.vue
@@ -1,9 +1,109 @@
 <template>
-  <body>
-    <NavComponentOwner/>
-    Notice
-    <FooterComponent/>
-  </body>
+<body>
+    <NavComponentOwner />
+    <!-- Announcement Banner -->
+    <div class="max-w-[85rem] mt-5 mb-5 px-4 sm:px-6 lg:px-8 mx-auto">
+        <div class="bg-blue-600 bg-no-repeat bg-cover bg-center p-4 rounded-md text-center">
+            <p class="mr-2 inline-block text-white">
+                Want to Give Notice To Your Building Residents?
+            </p>
+            <button onclick="my_modal_2.showModal()" class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-full border-2 border-white font-semibold text-white hover:bg-white/[.1] hover:border-white/[.1] focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-offset-2 transition-all text-sm" href="../figma.html">
+                Click Here
+                <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                </svg>
+            </button>
+        </div>
+    </div>
+    <!-- End Announcement Banner -->
+    <div class="h-auto">
+        <div>
+            <!-- Open the modal using ID.showModal() method -->
+
+            <dialog id="my_modal_2" class="modal">
+
+                <section class="modal-box overflow-hidden rounded-lg  shadow-2xl md:grid md:grid-cols-1">
+
+                    <div class="p-4 -mt-10 text-center sm:p-6 md:col-span-2 lg:p-8">
+                        <p class="text-sm font-semibold uppercase tracking-widest">
+                            <input type="text" hidden placeholder="Owner Name" v-model="owner_Id" class="input input-bordered w-full max-w-xs mt-2" />
+                        </p>
+                        <p class="text-sm font-semibold uppercase tracking-widest">
+                            <input type="text" placeholder="Building ID" v-model="post_notice.building_Id" class="input input-bordered w-full max-w-xs mt-2" />
+                        </p>
+                        <p class="text-sm font-semibold uppercase tracking-widest">
+                            <input type="text" placeholder="Title" v-model="post_notice.title" class="input input-bordered w-full max-w-xs mt-2" />
+                        </p>
+
+                        <p class="text-sm mt-2 font-semibold uppercase tracking-widest">
+                            <textarea class="textarea textarea-bordered textarea-lg w-full max-w-xs" v-model="post_notice.description" placeholder="Description"></textarea>
+                        </p>
+                        <p class="text-sm font-semibold uppercase tracking-widest">
+                            <input type="date" v-model="post_notice.date" placeholder="Date" class="input input-bordered w-full max-w-xs mt-2" />
+                        </p>
+
+                        <form method="dialog">
+                            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                            <button v-on:click="create_notice" class="btn mt-2 inline-block w-full bg-black py-4 text-sm font-bold uppercase tracking-widest text-white">Create</button>
+                        </form>
+                    </div>
+                </section>
+            </dialog>
+        </div>
+        <!--
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+-->
+
+        <div class="overflow-x-auto">
+            <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            Building ID
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            Title
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            Description
+                        </th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            Date
+                        </th>
+                        <th class="px-4 py-2"></th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200" v-for="item in notice" :key="item.id">
+                    <tr>
+                        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                            {{ item.building_Id }}
+                        </td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ item.title }}</td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ item.description }}</td>
+                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ item.date }}</td>
+                        <td class="whitespace-nowrap px-4 py-2">
+                            <a href="#" class="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
+                                Update
+                            </a>
+                            <a v-on:click="remove_notice(item.id)" href="#" class="inline-block rounded bg-indigo-600 ms-2 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
+                                Delete
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+                   
+
+                    
+
+    <FooterComponent />
+</body>
 </template>
 
 <script>
@@ -12,12 +112,68 @@ import FooterComponent from './FooterComponent.vue';
 import axios from 'axios';
 
 export default {
-name:"NoticeComponentOwner",
-components:{
-    NavComponentOwner,
-    FooterComponent
-}
-    
+    name: "NoticeComponentOwner",
+    data() {
+        return {
+            owner_Id: '',
+            notice: [],
+            post_notice:{
+                building_Id: '',
+                title: '',
+                description: '',
+                date: '',
+            }
+        }
+    },
+    components: {
+        NavComponentOwner,
+        FooterComponent
+    },
+    methods: {
+        check_owner() {
+            let users = localStorage.getItem('users-info');
+
+            if (!users) {
+                this.$router.push({
+                    name: 'LoginPageOwner'
+                })
+            } else {
+                this.owner_Id = JSON.parse(users).id;
+            }
+            // console.warn(this.owner_Id);
+        },
+        async get_notice_by_owner_Id() {
+            const get_notice_by_owner_Id = await axios.get("http://127.0.0.1:8000/api/Api/Notice/TableByOwnerID/" + this.owner_Id);
+            let responseData = get_notice_by_owner_Id.data.data
+            this.notice = responseData
+            console.warn(this.notice);
+        },
+        async create_notice(){
+            const create_notice = await axios.post("http://127.0.0.1:8000/api/Api/Notice/Create_Form_Post",{
+                owner_Id: this.owner_Id,
+                building_Id:this.post_notice.building_Id,
+                title: this.post_notice.title,
+                description: this.post_notice.description,
+                date: this.post_notice.date
+            })
+            if(create_notice.data.status == true){
+                this.get_notice_by_owner_Id();
+            }
+        },
+        async remove_notice(id) {
+            let remove_notice = await axios.delete("http://127.0.0.1:8000/api/Api/DeleteNotice/" + id)
+            if (remove_notice.status == 200) {
+                this.get_notice_by_owner_Id();
+            }
+
+        }
+
+    },
+    mounted() {
+        this.check_owner();
+        this.get_notice_by_owner_Id();
+    }
+
 }
 </script>
 
